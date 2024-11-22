c7c35a42195cfb5b43887a6db437c60726da87bf
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2024-11-22 13:24:17 -0300

    feat : add TransactionController class

---
+++ producer/src/main/java/com/springboot_kafka/transaction_infos_producer/controller/TransactionController.java
@@ -0,0 +1,25 @@
+package com.springboot_kafka.transaction_infos_producer.controller;
+
+import com.springboot_kafka.transaction_infos_producer.model.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> createTransaction(@RequestBody Transaction transaction){
+        kafkaTemplate.send("transaction-topic", transaction);
+        return ResponseEntity.ok().build();
+    }
+
+}
